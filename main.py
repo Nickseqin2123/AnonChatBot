16392f05ebdef55852e03932f7736d0843eceffe
Nickseqin2123 <bletigri@gmail.com>
2025-02-18 21:54:32 +0300

    end today

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram import Dispatcher, Bot
 from aiogram.filters.command import CommandStart
 from aiogram.types import Message
 from db.config import cfg
+from requestsdb.user import getUserLink, setUser
 
 
 dp = Dispatcher()
@@ -12,9 +13,28 @@ dp = Dispatcher()
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer(
-        text='Бот анон'
-    )
+    text = message.text.split('/start')
+    
+    if len(text) > 1:
+        await message.answer(
+            text='Напишите ему сообщение'
+        )  
+    else:
+        user_id = message.from_user.id
+        
+        user = await getUserLink(user_id)
+        
+        if isinstance(user, bool):
+            await setUser(user_id)
+        
+        user = await getUserLink(user_id)
+        
+        await message.answer(
+            text=f'''
+    Привет, я бот для анонимных сообщений. Вот твоя ссылка: https://t.me/anonimn_chat_cheb_bot?start={user}
+    Выложи её в свой канал или добавь в описание, чтобы получать анонимные вопросы.
+    Все данные конфиденциальны'''
+        )
 
 
 async def main():
--- requestsdb/user.py
+++ requestsdb/user.py
@@ -1,11 +1,11 @@
-import asyncio
-
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import async_sessionmaker
 from db.config import cfg
 from db.models import Users
+from uti.code import encode_id
 
 
-async def get_user(id_usr: int):
+async def getUserLink(id_usr: int):
     session = async_sessionmaker(cfg.engine)
     
     try:
@@ -15,9 +15,33 @@ async def get_user(id_usr: int):
             if bool(user):
                 return user.link
             else:
-                ...
+                return False
     finally:
         await cfg.engine.dispose()
 
 
-asyncio.run(get_user(1124518724))
+async def setUser(id_usr: int):
+    session = async_sessionmaker(cfg.engine)
+    
+    try:
+        async with session() as session:
+            encoded: str = await encode_id(id_usr)
+            
+            user = Users(id=id_usr, link=encoded)
+            session.add(user)
+            
+            await session.commit()  
+    finally:
+        await cfg.engine.dispose()
+
+
+async def searchUser(user_link: str):
+    session = async_sessionmaker(cfg.engine)
+    
+    try:
+        async with session() as session:
+            user = select(Users).filter(Users.link == user_link).all()
+            print(user)
+    finally:
+        await cfg.engine.dispose()
+
